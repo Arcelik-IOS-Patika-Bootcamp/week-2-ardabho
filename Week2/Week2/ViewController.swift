6bb64d0948de0e83854d41c2b27169fc636ee6b2
Arda Büyükhatipoğlu <ardabho@gmail.com>
2022-05-25 11:04:21 +0300

    Initial commit. Empty project

---
+++ Week2/Week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by Arda Büyükhatipoğlu on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
